d0b97cdbc6926f38fe367082301a62e55cf26058
Hirokazu NISHIOKA <hiro@nisshiee.org>
2013-05-28 00:13:06 +0900

    [ #4 ] Weakを確実に建てるTacticを実装

--- player/src/main/scala/ai/State.scala
+++ player/src/main/scala/ai/State.scala
@@ -4,6 +4,5 @@ import org.nisshiee.towerdefensescala._
 
 sealed trait State
 case object Init extends State
-case class Weak1(walls: List[Point], l: Labyrinth) extends State
-case class Weak2(walls: List[Point], l: Labyrinth) extends State
+case class Weak(walls: List[Point], l: Labyrinth) extends State
 case object Observe extends State
--- player/src/main/scala/ai/Tactic.scala
+++ player/src/main/scala/ai/Tactic.scala
@@ -5,31 +5,23 @@ import org.nisshiee.towerdefensescala._
 object Tactic {
 
   val basic: ((Snapshot, State)) => (Seq[Command], State) = { x =>
-    (init orElse weak1 orElse weak2)
+    (init orElse weak)
       .applyOrElse(x, default)
   }
 
   val init: PartialFunction[(Snapshot, State), (Seq[Command], State)] = {
     case (ss, Init) => Labyrinth.longest(ss.field) match {
-      case Some(l @ Labyrinth(_, w :: ws)) => (Command(w, WeakTower) :: Nil, Weak1(ws, l))
+      case Some(l @ Labyrinth(_, w1 :: w2 :: ws)) =>
+        (Command(w1, WeakTower) :: Command(w2, WeakTower) :: Nil, Weak(ws, l))
       case _ => (Nil, Observe)
     }
   }
 
-  val weak1: PartialFunction[(Snapshot, State), (Seq[Command], State)] = {
-    case (_, Weak1(Nil, _)) => (Nil, Observe)
-    case (_, Weak1(w :: Nil, _)) => (Command(w, WeakTower) :: Nil, Observe)
-    case (ss, Weak1(w :: ws, l)) if ss.level >= 29 => (Command(w, WeakTower) :: Nil, Weak2(ws, l))
-    case (ss, Weak1(w :: ws, l)) => (Command(w, WeakTower) :: Nil, Weak1(ws, l))
-  }
-
-  val weak2: PartialFunction[(Snapshot, State), (Seq[Command], State)] = {
-    case (_, Weak2(Nil, _)) => (Nil, Observe)
-    case (_, Weak2(w :: Nil, _)) => (Command(w, WeakTower) :: Nil, Observe)
-    case (_, Weak2(w1 :: w2 :: Nil, _)) =>
-      (Command(w1, WeakTower) :: Command(w2, WeakTower) :: Nil, Observe)
-    case (_, Weak2(w1 :: w2 :: ws, l)) =>
-      (Command(w1, WeakTower) :: Command(w2, WeakTower) :: Nil, Weak2(ws, l))
+  val weak: PartialFunction[(Snapshot, State), (Seq[Command], State)] = {
+    case (_, Weak(Nil, _)) => (Nil, Observe)
+    case (_, Weak(w :: Nil, l)) => (Command(w, WeakTower) :: Nil, Weak(Nil, l))
+    case (_, Weak(w1 :: w2 :: ws, l)) =>
+      (Command(w1, WeakTower) :: Command(w2, WeakTower) :: Nil, Weak(ws, l))
   }
 
   val default: ((Snapshot, State)) => (Seq[Command], State) = { _ => (Nil, Observe) }
